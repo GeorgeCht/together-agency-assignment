c2242f060efd504f4ee8d6ffe7ae5be0550b0088
George Cht <2hardforutoo@hotmail.gr>
2026-02-17 21:17:49 +0200

    revert(config): reverted nextconfig for basepath changes

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/together-agency-assignment",
-  assetPrefix: "/together-agency-assignment/",
   images: {
     unoptimized: true,
   },
